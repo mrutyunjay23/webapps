b7bdc3f533834040dea08f9e72696f274ff8ce43
Mrutyunjay <mrutyunjaychikmath@gmail.com>
2021-03-04 18:06:12 +0530

    whitenoise staticfiles storage added to settings

--- tailors/settings.py
+++ tailors/settings.py
@@ -126,5 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tailors/static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
