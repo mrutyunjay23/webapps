c205b5325143309f0293cb68616378e45f235774
mrutyunjay23 <mrutyunjaychikmath@gmail.com>
2021-04-02 20:41:51 +0530

    set environment variable to secret key

--- tailors/settings.py
+++ tailors/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'l0w7^7#5n()p_t#fwqos9g03%s^1j_2zz5yu==rot)pib9a!i'
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
